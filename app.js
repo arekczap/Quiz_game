a66f436cd4f45f45ee1d7a5231f335346b95377e
arekczap <arekczapski@gmail.com>
2020-08-31 18:52:27 +0200

    download data from firebase database, added event listeners for start button, added initialization function

--- app.js
+++ app.js
@@ -1,33 +1,82 @@
 import { database } from './firebase.js';
 
-const anwsers = document.querySelectorAll('.anwser');
-let randomQuestionNumber = Math.floor(Math.random() * 100) + 1;
-let question;
-
-//laczenie z baza danych
-var ref = database.ref('questions/' + randomQuestionNumber);
-ref.on('value', received);
-
-function received (data) {
-    var receivedQuestion = data.val();
+const selectors = {
+    questionContainerSelector: document.querySelector('.question-container'),
+    questionSelector: document.querySelector('.question'),
+    answerSelector1: document.querySelector('.anwser1'),
+    answerSelector2: document.querySelector('.anwser2'),
+    answerSelector3: document.querySelector('.anwser3'),
+    answerSelector4: document.querySelector('.anwser4'),
+    startButtonSelector: document.querySelector('.controls__start-button'),
+    restartButtonSelector: document.querySelector('.controls__next-button'),
+    scoreContainerSelector: document.querySelector('.score'),
+    scoreValueSelector: document.querySelector('.score__value'),
 };
 
-console.log(ref);
 
+(function initialFunction({ questionSelector, answerSelector1, answerSelector2, answerSelector3, answerSelector4, scoreValueSelector }) {
 
+    questionSelector.innerText = '';
+    answerSelector1.innerText = '';
+    answerSelector2.innerText = '';
+    answerSelector3.innerText = '';
+    answerSelector4.innerText = '';
+    scoreValueSelector.innerText = '0';
 
+})(selectors);
 
+selectors.startButtonSelector.addEventListener('click', (e) => {
+    const { questionContainerSelector, restartButtonSelector, scoreContainerSelector} = selectors;
 
-anwsers.forEach(anwser => {
-    anwser.addEventListener('click', (e) => {
-        e.toElement.classList.toggle('correct');
-    });
+    e.toElement.classList.toggle('hide');
+    questionContainerSelector.classList.toggle('hide');
+    restartButtonSelector.classList.toggle('hide');
+    scoreContainerSelector.classList.toggle('hide');
+
 });
 
 
+    let randomQuestionNumber = Math.floor(Math.random() * 100) + 1;
+
+
+    // laczenie z baza danych
+    var reference = database.ref('questions/' + randomQuestionNumber);
+    reference.on('value', getQuestionFromDB);
+
+
+
+    function getQuestionFromDB(data) {
+        const questionFromDB = data.val();
+        const { question, answer, correct } = questionFromDB;
+
+
+
+        selectors.questionSelector.innerText = question;
+
+
+
+    };
+
+
+// console.log(getQuestionFromDataBase());
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+// anwsers.forEach(anwser => {
+//     anwser.addEventListener('click', (e) => {
+//         e.toElement.classList.toggle('correct');
+//     });
+// });
 
