591a80a3a4e96b7639676f24efed5ddc00026409
arekczap <arekczapski@gmail.com>
2020-09-01 22:50:11 +0200

    download and load questions, events buttons click, code review, TODO: check correct answer and get correct bgc color

--- app.js
+++ app.js
@@ -3,80 +3,87 @@ import { database } from './firebase.js';
 const selectors = {
     questionContainerSelector: document.querySelector('.question-container'),
     questionSelector: document.querySelector('.question'),
-    answerSelector1: document.querySelector('.anwser1'),
-    answerSelector2: document.querySelector('.anwser2'),
-    answerSelector3: document.querySelector('.anwser3'),
-    answerSelector4: document.querySelector('.anwser4'),
+    answerSelector: document.querySelectorAll('.anwser'),
+    answerButtons: document.querySelectorAll('.answer-buttons .btn'),
     startButtonSelector: document.querySelector('.controls__start-button'),
     restartButtonSelector: document.querySelector('.controls__next-button'),
     scoreContainerSelector: document.querySelector('.score'),
     scoreValueSelector: document.querySelector('.score__value'),
 };
 
+function getQuestionFromDB() {
+    //generowanie losowej liczby do pytania
+    let randomQuestionNumber = Math.floor(Math.random() * 100) + 1;
 
-(function initialFunction({ questionSelector, answerSelector1, answerSelector2, answerSelector3, answerSelector4, scoreValueSelector }) {
+    // laczenie z baza danych
+    let reference = database.ref('questions/' + randomQuestionNumber);
+    reference.on('value', getQuestion);
+}
+
+// pobieranie pytania i odpowiedzi z bazy i wpisanie ich do odpowiednich pól
+function getQuestion(data) {
+    let questionFromDB = data.val();
+    const { question, answer, correct } = questionFromDB;
+
+    selectors.questionSelector.innerText = question;
+    selectors.answerSelector.forEach((selector, id) => {
+        selector.innerText = answer[id];
+    });
+
+    checkCorrectAnswer(correct);
+};
+
+//zerowanie wyniku
+function setZeroScore({ scoreValueSelector }) {
+    scoreValueSelector.innerText = '0';
+}
+
+//sprawdzenie poprawnej odpowiedzi
+function checkCorrectAnswer(correct) {
+
+    const { answerButtons, answerSelector } = selectors;
+
+    answerButtons.forEach(selector => {
+        selector.addEventListener('click', (e) => {
+
+            selector.innerText == correct ? selector.classList.add('correct') : selector.classList.add('wrong')
+        });
+
+
+        //TODO: zrobic podświetlanie elementów dobrych i złych
+    });
+
+}
+
+//funkcja inicjaloizująca,resetowanie pól tekstowych
+(function initialFunction({ questionSelector, answerSelector }) {
 
     questionSelector.innerText = '';
-    answerSelector1.innerText = '';
-    answerSelector2.innerText = '';
-    answerSelector3.innerText = '';
-    answerSelector4.innerText = '';
-    scoreValueSelector.innerText = '0';
-
+    setZeroScore(selectors);
+    answerSelector.forEach((answer => {
+        answer.innerText = '';
+    }));
+    getQuestionFromDB();
 })(selectors);
 
+//event nacisnięcia przycisku start, przejście do planszy z grą
 selectors.startButtonSelector.addEventListener('click', (e) => {
-    const { questionContainerSelector, restartButtonSelector, scoreContainerSelector} = selectors;
+    const { questionContainerSelector, restartButtonSelector, scoreContainerSelector } = selectors;
 
     e.toElement.classList.toggle('hide');
     questionContainerSelector.classList.toggle('hide');
     restartButtonSelector.classList.toggle('hide');
     scoreContainerSelector.classList.toggle('hide');
-
 });
 
 
-    let randomQuestionNumber = Math.floor(Math.random() * 100) + 1;
-
-
-    // laczenie z baza danych
-    var reference = database.ref('questions/' + randomQuestionNumber);
-    reference.on('value', getQuestionFromDB);
-
-
-
-    function getQuestionFromDB(data) {
-        const questionFromDB = data.val();
-        const { question, answer, correct } = questionFromDB;
-
-
-
-        selectors.questionSelector.innerText = question;
-
-
-
-    };
-
-
-// console.log(getQuestionFromDataBase());
+selectors.restartButtonSelector.addEventListener('click', () => {
+    getQuestionFromDB();
+    setZeroScore(selectors);
+});
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// anwsers.forEach(anwser => {
-//     anwser.addEventListener('click', (e) => {
-//         e.toElement.classList.toggle('correct');
-//     });
-// });
 
