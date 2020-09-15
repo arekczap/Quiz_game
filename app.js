ca186d9929e2d4938488924384337a72acef9062
arekczap <arekczapski@gmail.com>
2020-09-15 23:22:15 +0200

    working on async js in download question and logic

--- app.js
+++ app.js
@@ -6,52 +6,100 @@ const selectors = {
     answerSelector: document.querySelectorAll('.anwser'),
     answerButtons: document.querySelectorAll('.answer-buttons .btn'),
     startButtonSelector: document.querySelector('.controls__start-button'),
-    restartButtonSelector: document.querySelector('.controls__next-button'),
     scoreContainerSelector: document.querySelector('.score'),
     scoreValueSelector: document.querySelector('.score__value'),
     scoreUpdateSelector: document.querySelector('.score__update')
 };
 
 let totalScore;
-let correctQuestionFromDb = '';
+let questionFromDb;
+let correctAswerFromDb = '';
 
 
 function getQuestionFromDB() {
+    return new Promise((resolve) => {
+        let random = Math.floor(Math.random() * 100) + 1;
+        // laczenie z baza danych
+        let reference = database.ref('questions/' + random);
+        reference.on('value', (data) => {
+            questionFromDb = data.val();
+        });
+        resolve();
+    })
 
-    let random = Math.floor(Math.random() * 100) + 1;
-    // laczenie z baza danych
-    let reference = database.ref('questions/' + random);
-    reference.on('value', getQuestion, getError);
 
 }
 
+function attachQuestionToSelectors() {
+    return new Promise((resolve) => {
 
-function getError(error) {
-    console.log(error)
-
+        const { question, answer } = questionFromDb;
+        selectors.questionSelector.innerText = question;
+        selectors.answerSelector.forEach((selector, id) => {
+            selector.innerText = answer[id];
+        })
+        resolve()
+        // selectors.questionSelector.innerText = question;
+        // selectors.answerSelector.forEach((selector, id) => {
+        //     selector.innerText = answer[id];
+        // });
+    })
 }
-
-
-
-// pobieranie pytania i odpowiedzi z bazy i wpisanie ich do odpowiednich pól
-function getQuestion(data) {
-    let questionFromDB = data.val();
-    const { question, answer, correct } = questionFromDB;
-
-    correctQuestionFromDb = correct;
-    selectors.questionSelector.innerText = question;
-    selectors.answerSelector.forEach((selector, id) => {
-        selector.innerText = answer[id];
-    });
-};
-
-
 
 //zmiana wyniku
 function updateScoreNumber(numberToChangeScore) {
     const { scoreValueSelector } = selectors;
     totalScore += numberToChangeScore;
     scoreValueSelector.textContent = totalScore;
+}
+
+function timeoutCheckAnswer() {
+    return new Promise((resolve) => {
+        const { answerButtons, answerSelector, scoreUpdateSelector } = selectors;
+
+        event.toElement.classList.add('checked');
+
+        setTimeout(() => {
+            answerSelector.forEach(sel => {
+                if (sel.textContent == correctAswerFromDb) {
+                    sel.classList.add('correct');
+
+                    scoreUpdateSelector.classList.add('update');
+                }
+                else {
+                    sel.classList.add('wrong');
+                    scoreUpdateSelector.classList.add('update');
+                }
+                sel.classList.remove('checked')
+            });
+            resolve()
+        }, 3000);
+    })
+}
+
+function clearAddedClassNames() {
+    return new Promise((resolve) => {
+        const { answerSelector } = selectors;
+
+
+        setTimeout(() => {
+            answerSelector.forEach(sel => {
+                sel.classList.remove('correct');
+                sel.classList.remove('wrong');
+            })
+            resolve();
+        }, 3000);
+    })
+}
+
+function nextQuestion(params) {
+    return new Promise((resolve) => {
+
+        getQuestionFromDB();
+        resolve();
+    })
+
+
 }
 
 
@@ -63,88 +111,64 @@ selectors.answerButtons.forEach(selector => {
 });
 
 
-function checkCorrectAnswer() {
+async function checkCorrectAnswer() {
 
-    console.log(correctQuestionFromDb);
-    const { answerButtons, answerSelector, scoreUpdateSelector } = selectors;
-
-//poprawić skrypt dobrego pytania po zmianach algorytmu
-    getQuestionFromDB();
-    // e.toElement.classList.add('checked');
-    // setTimeout(() => {
-
-    //     answerSelector.forEach(sel => {
-    //         if (sel.textContent == correct) {
-    //             sel.classList.add('correct');
-
-    //             scoreUpdateSelector.classList.add('update');
-    //         }
-    //         else {
-    //             sel.classList.add('wrong');
-    //             scoreUpdateSelector.classList.add('update');
-    //         }
-    //     });
-    //     e.toElement.classList.remove('checked');
-
-    // }, 3000);
-
-
+    await timeoutCheckAnswer();
+    await clearAddedClassNames();
+    await nextQuestion();
     // setTimeout(() => {
     //     e.toElement.textContent == correctAnswerFromDB ? updateScoreNumber(10) : updateScoreNumber(-7);
     //     scoreUpdateSelector.classList.remove('update');
 
     // }, 4000);
-
-
-
-    // setTimeout(() => {
-    //     // clearAddedClassNames();
-    //     
-    // }, 5000);
-
-
-
 }
 
-function clearAddedClassNames() {
-    const { answerSelector } = selectors;
-
-    answerSelector.forEach(sel => {
 
 
-        sel.classList.remove('correct');
-        sel.classList.remove('wrong');
-    })
-}
 
-//funkcja inicjaloizująca,resetowanie pól tekstowych
 function initialFunction() {
+    return new Promise((resolve) => {
+        const { questionSelector, answerSelector } = selectors;
+        questionSelector.innerText = '';
+        answerSelector.forEach((answer => {
+            answer.innerText = '';
+        }));
+        totalScore = 0;
+        resolve
+    })
 
-    const { questionSelector, answerSelector } = selectors;
-    questionSelector.innerText = '';
-    answerSelector.forEach((answer => {
-        answer.innerText = '';
-    }));
-
-    totalScore = 0;
-    getQuestionFromDB();
 };
 
-// event nacisnięcia przycisku start, przejście do planszy z grą
+
+
+
+
 selectors.startButtonSelector.addEventListener('click', (e) => {
-    const { questionContainerSelector, restartButtonSelector, scoreContainerSelector } = selectors;
 
-    e.toElement.classList.toggle('hide');
-    questionContainerSelector.classList.toggle('hide');
-    restartButtonSelector.classList.toggle('hide');
-    scoreContainerSelector.classList.toggle('hide');
-    initialFunction();
+    e.toElement.disabled = true;
+    async function fc() {
+        await getQuestionFromDB();
+        await initialFunction();
+        await attachQuestionToSelectors();
+    }
+    
+
+    
+
+    // console.log(res);
+
+    setTimeout(() => {
+        const { questionContainerSelector, scoreContainerSelector } = selectors;
+
+        e.toElement.classList.toggle('hide');
+        questionContainerSelector.classList.toggle('hide');
+        scoreContainerSelector.classList.toggle('hide');
+
+
+    }, 500);
+
+
+
 });
 
 
-selectors.restartButtonSelector.addEventListener('click', () => {
-    getQuestionFromDB();
-    totalScore = 0;
-    selectors.scoreValueSelector.textContent = totalScore;
-
-});
